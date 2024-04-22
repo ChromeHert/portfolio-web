ba0f3435ab5207dee2d37f09c8ae4f13313cd49c
unknown <ariyeric@gmail.com>
2024-04-22 13:57:53 +0100

    created the hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
+import heroimage from '../assets/heroimage.png'
+
+const Hero = () => {
+  return (
+    <section class="pt-10 md:pt-20">
+    <div class="container mx-auto px-8 flex flex-col md:flex-row items-center">
+      <div class="text-center md:text-left md:w-1/2">
+        <h1 class="text-4xl lg:text-5xl xl:text-6xl font-bold leading-8">
+          Welcome To My Portfolio <br /> 
+          <TypeAnimation 
+            sequence={[
+                "I'm A Frontend Developer",
+                1000,
+                "HTML & CSS",
+                1000,
+                "Bootstrap & Tailwind",
+                1000,
+                "JavaScript",
+                1000,
+                "React JS",
+                1000,
+            ]}
+            wrapper='span'
+            speed={60}
+            repeat={Infinity}
+          />
+        </h1>
+      </div>
+      <div class="md:w-auto md:ml-8">
+        <img src={heroimage} alt="hero image" />
+      </div>
+    </div>
+  </section>
+  
+  );
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {AiOutlineClose, AiOutlieMenu} from 'react-icons/ai'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
 
@@ -9,15 +9,15 @@ const Navbar = () => {
         setNav(!nav)
     }
   return (
-    <div className='bg-black h-[100px] max-w-[1200px] mx-auto flex justify-between items-center'>
+    <div className='bg-black h-[100px] mx-auto text-white flex justify-between items-center'>
         <h1 className='text-3xl font-medium primary-color ml-4'>@Eric</h1>
         <ul className='hidden md:flex'>
             <li className='p-5'><a href="about">About</a></li>
             <li className='p-5'><a href="work">Work</a></li>
             <li className='p-5'><a href="contact">Contact</a></li>
         </ul>
-        <div onClick={handleNav} className='block md:hidden'> 
-            {nav ? <AiOutlineClose size={20}/> : <AiOutlieMenu size={20}/>}
+        <div onClick={handleNav} className='block md:hidden mr-5'> 
+            {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         </div>
 
         <div className={nav ? 'fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500' : 'fixed left-[-100%]' }>
